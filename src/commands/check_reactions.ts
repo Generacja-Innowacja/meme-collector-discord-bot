59c1e56527b22db2a00f6463db4384a859d41d01
LunarWanderer <lunarwanderer13@gmail.com>
2025-12-25 17:53:28 +0100

    Add support for choosing other users

--- src/commands/check_reactions.ts
+++ src/commands/check_reactions.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction, EmbedBuilder } from "discord.js"
+import { SlashCommandBuilder, ChatInputCommandInteraction, EmbedBuilder, User } from "discord.js"
 import { Command } from "../command"
 import { ScoreEntry, EmojiObject } from "./../index"
 import fs from "fs"
@@ -17,28 +17,38 @@ export const CheckReactions: Command = {
                     { name: "Rok", value: "yearly" }
                 )
                 .setRequired(false)
+        )
+        .addUserOption(option =>
+            option
+                .setName("target")
+                .setDescription("Kogo reakcje chcesz dostać")
         ),
 
     async execute(interaction: ChatInputCommandInteraction) {
         type ScoreType = "weekly" | "monthly" | "yearly"
         const score_type: ScoreType = (interaction.options.getString("type", false) as ScoreType) ?? "weekly"
         const scores: ScoreEntry[] = JSON.parse(fs.readFileSync("src/logs/memes.json", "utf-8"))
-        const entry: ScoreEntry | undefined = scores.find(e => e.id === interaction.user.id)
+
+        const target: User = interaction.options.getUser("target") ?? interaction.user
+        const entry: ScoreEntry | undefined = scores.find(e => e.id === target.id)
 
         const embed = new EmbedBuilder()
             .setColor("#ff6432")
 
+        let title: string = ""
+        target === interaction.user ? title = "Twoje reakcje z tego " : title = `Reakcje ${target.username} z tego `
         switch (score_type) {
             case "weekly":
-                embed.setTitle("Twoje reakcje z tego tygodnia:")
+                title += "tygodnia"
                 break
             case "monthly":
-                embed.setTitle("Twoje reakcje z tego miesiąca:")
+                title += "miesiąca"
                 break
             case "yearly":
-                embed.setTitle("Twoje reakcje z tego roku:")
+                title += "roku"
                 break
         }
+        embed.setTitle(title)
 
         if (entry && entry.reactions[score_type].length > 0) {
             let description: string = ""
@@ -51,7 +61,7 @@ export const CheckReactions: Command = {
             embed.setDescription("Brak! Zacznij wrzucać memy i rozkręcać serwer zamiast tylko dawania serduszek!")
         }
 
-        embed.setThumbnail(interaction.user.displayAvatarURL())
+        embed.setThumbnail(target.displayAvatarURL())
 
         await interaction.reply({ embeds: [embed]})
     }
